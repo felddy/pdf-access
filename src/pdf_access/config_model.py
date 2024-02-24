78d9601f6a401245462f9ece17d54b1f5aee36af
Felddy <markf@geekpad.com>
2024-02-24 16:55:59 -0500

    Forbid unknown parameters in config

--- src/pdf_access/config_model.py
+++ src/pdf_access/config_model.py
@@ -1,18 +1,23 @@
 from typing import Any, Dict, List
 import re
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Extra, Field, validator
+from pathlib import Path
 
 
 class Action(BaseModel):
-    args: Dict[str, Any] = Field(default_factory=dict)
     action: str
+    args: Dict[str, Any] = Field(default_factory=dict)
+
+    class Config:
+        extra = Extra.forbid
 
 
 class Plan(BaseModel):
-    metadata_search: Dict[str, str]
-    path_regex: re.Pattern = re.compile(".*")
+    actions: List[str]  # TODO link to action objects
+    metadata_search: Dict[str, str]  # TODO precompile regex
     passwords: List[str] = Field(default_factory=list)
-    actions: List[str]
+    path_regex: re.Pattern = re.compile(".*")
+    post_process: List[str] = Field(default_factory=list)
 
     @validator("path_regex", pre=True)
     def compile_path_regex(cls, v):
@@ -20,16 +25,30 @@ class Plan(BaseModel):
             return re.compile(v)
         return v
 
+    class Config:
+        extra = Extra.forbid
+
 
 class Source(BaseModel):
-    in_path: str
-    out_path: str
+    in_path: Path
+    out_path: Path
     out_suffix: str = Field("")
-    post_process: List[str] = Field(default_factory=list)
     plans: List[str]
+
+    @validator("in_path", "out_path")
+    def convert_to_path(cls, v):
+        if isinstance(v, str):
+            return Path(v)
+        return v
+
+    class Config:
+        extra = Extra.forbid
 
 
 class Config(BaseModel):
     actions: Dict[str, Action]
     plans: Dict[str, Plan]
     sources: Dict[str, Source]
+
+    class Config:
+        extra = Extra.forbid
