362f68b633dabdab347587a0e9972e62129d6258
Felddy <markf@geekpad.com>
2024-02-26 17:50:30 -0500

    Remove logging level tests

--- tests/test_pdf_access.py
+++ tests/test_pdf_access.py
@@ -2,7 +2,6 @@
 """Tests for pdf access."""
 
 # Standard Python Libraries
-import logging
 import os
 import sys
 from unittest.mock import patch
@@ -51,7 +50,7 @@ def test_running_as_module(capsys):
             import pdf_access.__main__  # noqa: F401
     captured = capsys.readouterr()
     assert (
-        captured.out == f"{PROJECT_VERSION}\n"
+        captured.out == f"bogus {PROJECT_VERSION}\n"
     ), "standard output by '--version' should agree with module.__version__"
 
 
@@ -63,37 +62,3 @@ def test_release_version():
     assert (
         RELEASE_TAG == f"v{PROJECT_VERSION}"
     ), "RELEASE_TAG does not match the project version"
-
-
-@pytest.mark.parametrize("level", log_levels)
-def test_log_levels(level):
-    """Validate commandline log-level arguments."""
-    with patch.object(sys, "argv", ["bogus", f"--log-level={level}", "1", "1"]):
-        with patch.object(logging.root, "handlers", []):
-            assert (
-                logging.root.hasHandlers() is False
-            ), "root logger should not have handlers yet"
-            return_code = None
-            try:
-                pdf_access.pdf_access.main()
-            except SystemExit as sys_exit:
-                return_code = sys_exit.code
-            assert return_code is None, "main() should return success"
-            assert (
-                logging.root.hasHandlers() is True
-            ), "root logger should now have a handler"
-            assert (
-                logging.getLevelName(logging.root.getEffectiveLevel()) == level.upper()
-            ), f"root logger level should be set to {level.upper()}"
-            assert return_code is None, "main() should return success"
-
-
-def test_bad_log_level():
-    """Validate bad log-level argument returns error."""
-    with patch.object(sys, "argv", ["bogus", "--log-level=emergency", "1", "1"]):
-        return_code = None
-        try:
-            pdf_access.pdf_access.main()
-        except SystemExit as sys_exit:
-            return_code = sys_exit.code
-        assert return_code == 1, "main() should exit with error"
